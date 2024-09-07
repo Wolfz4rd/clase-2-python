1b87b62dabc9eaed2d5cfc219bf01777c0dd5c23
Miguel Añez <migueaanez@gmail.com>
2024-09-07 09:48:45 -0400

    first commit

---
+++ funciones.py
@@ -0,0 +1,24 @@
+# def nombredefuncion(parametros):
+
+nombre = "miguel"
+num_1 = 2
+num_2 = 3
+
+def saludo():
+    print("hola mucho gusto"+ nombre)
+
+def sumar():
+    numero_1 = 10
+    numero_2 = 20
+    # print(numero_1+numero_2)
+    suma = numero_1 + numero_2
+    return suma
+
+def Resta(num_1, num_2):
+
+    resta = num_1 - num_2
+    return resta
+
+print(Resta(num_1,num_2)+10)
+sumar()
+# saludo()
---
+++ venezuela.py
@@ -0,0 +1,7 @@
+from canada import numero_2
+
+numero_1 = 10
+
+suma = numero_1 + numero_2
+
+print (suma)
